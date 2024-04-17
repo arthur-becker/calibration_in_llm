dbcb33914f38b56ee7deed6b238f4e6ccacb1ebd
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-04-17 16:18:31 +0200

    install dependencies for benchmarking

---
+++ tools/benchmark/install.sh
@@ -0,0 +1,18 @@
+### This script installs the necessary dependencies for the benchmarking tools.
+### 
+### Instead of installing fron requirements.txt, we install from git repos.
+### The reason is that we probably need to specify some environmental variables
+### to enable CUDA support, and we can't do that in requirements.txt.
+
+LLAMA_CPP_PYTHON_REPO="https://github.com/arthur-becker/calibrated-llama-cpp-python.git@add-calibration"
+LM_EVALUATION_HARNESS_REPO="https://github.com/EleutherAI/lm-evaluation-harness.git@8b326be7b8307c73ef4dd4486291b7473d8a6e92"
+
+pip install git+$LLAMA_CPP_PYTHON_REPO
+echo "LLAMA_CPP_PYTHON installed\n"
+
+pip install 'llama-cpp-python[server]'
+
+pip install git+$LM_EVALUATION_HARNESS_REPO
+echo "LM_EVALUATION_HARNESS installed\n"
+
+echo "\nAll dependencies installed"
