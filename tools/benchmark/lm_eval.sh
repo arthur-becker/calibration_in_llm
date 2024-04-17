164de5635429b0fd9fa0488a9bdbb32eb32cce0a
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-04-17 22:39:22 +0200

    change task from mmlu to gsm8k

--- tools/benchmark/lm_eval.sh
+++ tools/benchmark/lm_eval.sh
@@ -12,7 +12,7 @@ fi
 echo "\nLLAMA_CPP_URL: $LLAMA_CPP_URL"
 
 echo "\n\nRunning the evaluation harness with the following parameters:"
-echo "> python3 -m lm_eval --model gguf --tasks mmlu_flan_cot_fewshot --model_args base_url=$LLAMA_CPP_URL \n\n"
+echo "> python3 -m lm_eval --model gguf --tasks gsm8k_cot_self_consistency --model_args base_url=$LLAMA_CPP_URL \n\n"
 lm_eval --model gguf \
-        --tasks mmlu_flan_cot_fewshot \
+        --tasks gsm8k_cot_self_consistency \
         --model_args base_url=$LLAMA_CPP_URL
