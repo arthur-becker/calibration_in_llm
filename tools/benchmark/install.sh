c9ece1c69f81709033b577f462528f5cadbb2d5a
Arthur Becker <arthur.becker@rwth-aachen.de>
2024-04-17 21:18:12 +0200

    install forked modified version of lm_evaluation_harness

--- tools/benchmark/install.sh
+++ tools/benchmark/install.sh
@@ -5,7 +5,7 @@
 ### to enable CUDA support, and we can't do that in requirements.txt.
 
 LLAMA_CPP_PYTHON_REPO="https://github.com/arthur-becker/calibrated-llama-cpp-python.git@add-calibration"
-LM_EVALUATION_HARNESS_REPO="https://github.com/EleutherAI/lm-evaluation-harness.git@8b326be7b8307c73ef4dd4486291b7473d8a6e92"
+LM_EVALUATION_HARNESS_REPO="https://github.com/arthur-becker/lm-evaluation-harness.git@llama-temperature-sampling"
 
 pip install git+$LLAMA_CPP_PYTHON_REPO
 echo "LLAMA_CPP_PYTHON installed\n"
